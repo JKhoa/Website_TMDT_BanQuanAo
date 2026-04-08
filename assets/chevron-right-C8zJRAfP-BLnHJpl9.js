d6c0a4798aa901e6125160a43c1aff09f5016a6f
whuy-2212378 <whuy-2212378@example.com>
2026-04-08 12:50:20 +0700

    fix: correct production base path for github pages routes

---
+++ assets/chevron-right-C8zJRAfP-BLnHJpl9.js
@@ -0,0 +1,6 @@
+import{p as t}from"./index-FcQcseN0.js";/**
+ * @license lucide-react v0.487.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const h=[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]],p=t("chevron-right",h);export{p as c};
